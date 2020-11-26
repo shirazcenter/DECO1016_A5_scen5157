50966a7988553eb9f2b157e48697e9307d5abcea
ShirazCenter <scen5157@uni.sydney.edu.au>
2020-11-26 06:54:39 +0000

    nav tweak

--- script.js
+++ script.js
@@ -54,23 +54,18 @@ function filterFunction() {
   }
 }
 
-//JUMP TO DONATE FORM BUTTON: adapted from (https://www.w3schools.com/howto/howto_js_scroll_to_top.asp)
-//Get the button:
-mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
-    mybutton.style.display = "block";
+
+
+
+
+//NAV BAR
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
   } else {
-    mybutton.style.display = "none";
+    x.className = "topnav";
   }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 1000; // For Safari
-  document.documentElement.scrollTop = 1000; // For Chrome, Firefox, IE and Opera
 }
